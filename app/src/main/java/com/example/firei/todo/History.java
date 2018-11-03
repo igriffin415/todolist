29a34a98fac993bf3c941d6f834085afc3d2d4e2
igriffin415 <fireice16@yahoo.com>
2018-11-03 10:18:28 -0400

    its bo time baby

---
+++ app/src/main/java/com/example/firei/todo/History.java
@@ -0,0 +1,60 @@
+package com.example.firei.todo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class History extends AppCompatActivity {
+
+    public static ArrayList<String> TODO = new ArrayList<String>();
+    public static ArrayList<String> HISTORY = new ArrayList<String>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        System.out.println("** START **");
+        super.onCreate(savedInstanceState);
+        Intent intent = getIntent();
+        TODO = (ArrayList) intent.getSerializableExtra("todo");
+        HISTORY = (ArrayList) intent.getSerializableExtra("history");
+        setContentView(R.layout.activity_history);
+
+        final LinearLayout list = (LinearLayout) findViewById(R.id.historyList);
+        for(int i = 0; i < HISTORY.size(); i++){
+            String text = HISTORY.get(i);
+            createCheckbox(list, text);
+        }
+
+        Button ret = (Button) findViewById(R.id.ret);
+        ret.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.putExtra("todo", TODO);
+                intent.putExtra("history", HISTORY);
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
+
+    }
+
+    private LinearLayout createCheckbox(final LinearLayout list, String text){
+        CheckBox ch = new CheckBox(History.this);
+        ch.setText(text);
+        list.addView(ch);
+        ch.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                HISTORY.remove(((CheckBox) v).getText().toString());
+                TODO.add(((CheckBox) v).getText().toString());
+                list.removeView(v);
+            }
+        });
+        return list;
+    }
+}
---
+++ app/src/main/java/com/example/firei/todo/Todo.java
@@ -0,0 +1,145 @@
+package com.example.firei.todo;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class Todo extends AppCompatActivity {
+
+
+    public static ArrayList<String> TODO = new ArrayList<String>();
+    public static ArrayList<String> HISTORY = new ArrayList<String>();
+
+    final static int HISTORY_ACTION = 1;
+    final static int ITEM_ACTION = 2;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        load();
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_todo);
+
+        final LinearLayout list = (LinearLayout) findViewById(R.id.list);
+        if (savedInstanceState != null) {
+            TODO = (ArrayList) savedInstanceState.getSerializable("todo");
+            HISTORY = (ArrayList) savedInstanceState.getSerializable("history");
+        }
+        setList();
+
+        final EditText itemName = (EditText) findViewById(R.id.nameItem);
+        Button addNew = (Button) findViewById(R.id.add);
+        addNew.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String text = itemName.getText().toString();
+                createCheckbox(list, text);
+                TODO.add(text);
+            }
+        });
+
+        Button history = (Button) findViewById(R.id.history);
+        history.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(Todo.this, History.class);
+                intent.putExtra("todo", TODO);
+                intent.putExtra("history", HISTORY);
+                startActivityForResult(intent, 1);
+            }
+        });
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == HISTORY_ACTION) {
+            if (resultCode == RESULT_OK) {
+                TODO = (ArrayList) data.getSerializableExtra("todo");
+                HISTORY = (ArrayList) data.getSerializableExtra("history");
+                setList();
+            }
+        }
+        if (requestCode == ITEM_ACTION) {
+            if (resultCode == RESULT_OK) {
+                TODO = (ArrayList) data.getSerializableExtra("todo");
+            }
+        }
+    }
+
+    private void createCheckbox(final LinearLayout list, String text){
+        CheckBox ch = new CheckBox(Todo.this);
+        ch.setText(text);
+        list.addView(ch);
+        ch.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if(HISTORY.size() == 10){
+                    HISTORY.remove(0);
+                }
+                HISTORY.add(((CheckBox) v).getText().toString());
+                TODO.remove(((CheckBox) v).getText().toString());
+                list.removeView(v);
+            }
+        });
+    }
+
+    private void setList(){
+        final LinearLayout list = (LinearLayout) findViewById(R.id.list);
+        list.removeAllViews();
+        for(int i = 0; i < TODO.size(); i++){
+            String text = TODO.get(i);
+            createCheckbox(list, text);
+        }
+    }
+
+    @Override
+    public void onPause() {
+        //https://stackoverflow.com/questions/10523801/java-save-binary-code-to-a-file
+
+        try {
+            FileOutputStream outputStream = openFileOutput("TODO.dat", Context.MODE_PRIVATE);
+            ObjectOutputStream out = new ObjectOutputStream(outputStream);
+            out.writeObject((Serializable) TODO);
+            out.close();
+            outputStream.close();
+            System.out.println("OUTPUT STREAM CLOSED");
+        } catch (Exception e) {
+            System.out.println("BAD");
+            e.printStackTrace();
+        }
+
+        super.onPause();
+
+    }
+
+    private void load(){
+        try{
+            // Reading the object from a file
+            FileInputStream file = openFileInput("TODO.dat");
+            ObjectInputStream in = new ObjectInputStream(file);
+            // Method for deserialization of object
+            TODO = (ArrayList)in.readObject();
+            in.close();
+            file.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putSerializable("todo", TODO);
+        outState.putSerializable("history", HISTORY);
+        super.onSaveInstanceState(outState);
+    }
+}
